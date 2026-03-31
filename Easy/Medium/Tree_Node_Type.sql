c1b60bfb005a7d7bbab956bc22a030c529700f4a
Pratik Manure <162863265+Pratikmanure@users.noreply.github.com>
2026-03-31 20:37:08 +0530

    Create Tree_Node_Type.sql

---
+++ Easy/Medium/Tree_Node_Type.sql
@@ -0,0 +1,11 @@
+-- Problem:
+-- Classify each node as Root, Inner, or Leaf.
+
+-- Solution:
+SELECT id,
+CASE
+    WHEN p_id IS NULL THEN 'Root'
+    WHEN id IN (SELECT p_id FROM Tree) THEN 'Inner'
+    ELSE 'Leaf'
+END AS type
+FROM Tree;
