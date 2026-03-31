a229e20e1b4706a34f8ea01fd91a566fb4e735f8
Pratik Manure <162863265+Pratikmanure@users.noreply.github.com>
2026-03-31 20:39:55 +0530

    Create First_Year_Sales.sql

---
+++ Easy/Medium/First_Year_Sales.sql
@@ -0,0 +1,11 @@
+-- Problem:
+-- Find first year of sales for each product.
+
+-- Solution:
+SELECT product_id, year AS first_year, quantity, price
+FROM Sales
+WHERE (product_id, year) IN (
+    SELECT product_id, MIN(year)
+    FROM Sales
+    GROUP BY product_id
+);
