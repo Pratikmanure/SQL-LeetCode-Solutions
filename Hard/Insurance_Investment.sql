7aed296b7e69ae533701db971f00ea8a843f54b3
Pratik Manure <162863265+Pratikmanure@users.noreply.github.com>
2026-08-14 20:46:57 +0530

    Add files via upload

---
+++ Hard/Insurance_Investment.sql
@@ -0,0 +1,20 @@
+-- Problem:
+-- Find total investment value for records where:
+-- - tiv_2015 appears more than once
+-- - (lat, lon) pair is unique
+
+-- Solution:
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+)
+AND (lat, lon) IN (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+);
