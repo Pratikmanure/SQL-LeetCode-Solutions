86e9f83db49377f51da6786cfdd076baf7d996b4
Pratik Manure <162863265+Pratikmanure@users.noreply.github.com>
2026-08-14 20:47:42 +0530

    Delete Easy/Medium directory

--- Easy/Medium/Employees_Earning_More.sql
+++
@@ -1,9 +0,0 @@
--- Problem:
--- Find employees who earn more than their managers.
-
--- Solution:
-SELECT e.name AS Employee
-FROM Employee e
-JOIN Employee m
-ON e.managerId = m.id
-WHERE e.salary > m.salary;
